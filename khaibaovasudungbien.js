14f9b13e0352bead49266e3ef7cf1bad2929b0f6
Yuhhna2907 <luonganhhuy2004@gmail.com>
2025-09-15 10:25:43 +0700

    Add files via upload

---
+++ khaibaovasudungbien.js
@@ -0,0 +1,38 @@
+let i = 10
+let f =20.5
+let b = true
+let s = 'Ha Noi'
+
+document.write('i='+i)
+document.write('<br>')
+document.write('f='+f)
+document.write('<br>')
+document.write('b='+b)
+document.write('<br>')
+document.write('s='+s)
+
+let width =20
+  let height =20
+  let area = width*height
+  document.write('area='+area+)
+
+let a = (prompt("Nhập số a"))
+let b = (prompt('Nhập số b'))
+
+if (b===0){
+  alert('Không thể chia cho 0 ')
+}
+else{
+  if (a%b === 0) {
+    alert(a+'là bội số của b')
+  }
+  else{
+    alert(a+'không là bội số của b')
+  }
+}
+
+
+
+
+
+
