52f76e52635c423101693df2712d2d289d9927e0
Yuhhna2907 <luonganhhuy2004@gmail.com>
2025-09-15 14:24:54 +0700

    Add files via upload

---
+++ bien_kieudulieu_toantu.js
@@ -0,0 +1,19 @@
+//let x= parseInt(prompt("Điểm Vật lý là:"));
+//let y= parseInt(prompt('Điểm Hóa học là:'));
+//let z= parseInt(prompt("Điểm Sinh học là:"));
+//let sum=x+y+z;
+//let average=sum/3;
+//document.write('Tổng điểm ba môn là:'+ sum)
+//document.write('Điểm trung bình ba môn là:'+ average)
+
+//let doC=parseInt(prompt('Nhập độ C:'))
+//let doF=(doC* 9 / 5 ) + 32
+//document.write('Nhiệt độ độ F là'+ " " + doF)
+
+let r=parseInt(prompt("Nhập bán kính :"))
+const PI=3.14
+let areacircle=PI*r*r
+let perimeter=2*PI*r
+document.write("Diện tích hình tròn là:"+" " + areacircle)
+document.write('Chu vi hình tròn là:'+""+ perimeter)
+
